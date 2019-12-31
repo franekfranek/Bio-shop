8f0fbe815ee9f45ebeb382e97288b910305cf9ca
franekfranek <franciszekezawadzki@gmail.com>
2019-12-31 16:23:06 +0100

    jquery deleted js instead

--- ShopCart/wwwroot/js/site.js
+++ ShopCart/wwwroot/js/site.js
@@ -1,8 +1,22 @@
-﻿$(function () {
+﻿//$(function () {
 
-    if ($("a.confirmDeletion").length) {
-        $("a.confirmDeletion").click(() => {
-            if (!confirm("Confirm deletion")) return false;
-        })
-    }
-});
+//    if ($("a.confirmDeletion").length) {
+//        $("a.confirmDeletion").click(() => {
+//            if (!confirm("Confirm deletion")) return false;
+//        })
+//    }
+//});
+
+let deleteElements = document.getElementsByClassName("confirmDeletion");
+
+for (var i = 0; i < deleteElements.length; i++) {
+    deleteElements[i].addEventListener('click', displayConfirmation);
+}
+
+function displayConfirmation() {
+    if (window.confirm("Do you really want to delete this element?")) {
+        return true;
+    } else return false;
+
+}
+

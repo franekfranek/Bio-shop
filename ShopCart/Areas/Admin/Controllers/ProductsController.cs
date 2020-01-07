20cf96603686a1905317d0f6d1692b3dfd1f0419
franekfranek <franciszekezawadzki@gmail.com>
2020-01-07 01:35:35 +0100

    Admin- Edit and delete page for PRODUCTS

--- ShopCart/Areas/Admin/Controllers/ProductsController.cs
+++ ShopCart/Areas/Admin/Controllers/ProductsController.cs
@@ -96,6 +96,118 @@ namespace ShopCart.Areas.Admin.Controllers
 
             return View(product);
         }
+        //admin/products/details/{id}
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Product product = await context.Products.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        //admin/products/edit/{id}
+        public async Task<IActionResult> Edit(int id)
+        {
+            Product product = await context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name",product.CategoryId);
+
+
+            return View(product);
+        }
+
+        //POST  admin/products/edit/{id}        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name", product.CategoryId);
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                //IT checks if slug exists NOT to have products with the same name!
+                var slug = await context.Products.Where(x => x.Id != id).FirstOrDefaultAsync(x => x.Slug == product.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product already exists.");
+                    return View(product);
+                }
+
+                
+                
+                if (product.ImageUpload != null)
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+
+                    if(!string.Equals(product.Image, "noimage.png")) //if it is not noimage then we have to delete the image
+                    {
+                        string oldImagePath = Path.Combine(uploadsDir, product.Image);
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+
+                    }
+
+                    string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName; // this gives unique id so no same image twice uploaded 
+                    string filePath = Path.Combine(uploadsDir, imageName);
+                    FileStream fileStream = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fileStream);
+                    fileStream.Close();
+                    product.Image = imageName;
+                }
+                
+                context.Update(product);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been edited.";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        //GET /admin/products/delete/{id}
+        public async Task<IActionResult> Delete(int id)
+        {
+            Product product = await context.Products.FindAsync(id);
+
+
+            if (product == null)
+            {
+                TempData["Error"] = "The product does not exist.";
+            }
+            else
+            {
+                if (!string.Equals(product.Image, "noimage.png")) //if it is not noimage then we have to delete the image
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    string oldImagePath = Path.Combine(uploadsDir, product.Image);
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+
+                }
+                context.Products.Remove(product);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been removed.";
+            }
+
+            return RedirectToAction("Index");
+        }
 
     }
 }

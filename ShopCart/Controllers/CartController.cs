95e31e0e9eced0fc5e72fa2147ec874fc49cfa42
franekfranek <franciszekezawadzki@gmail.com>
2020-01-08 22:06:42 +0100

    cart index method and cart ViewModel

--- ShopCart/Controllers/CartController.cs
+++ ShopCart/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ShopCart.Infrastructure;
+using ShopCart.Models;
 
 namespace ShopCart.Controllers
 {
@@ -15,9 +16,23 @@ namespace ShopCart.Controllers
         {
             this.context = context;
         }
+        // GET /cart
         public IActionResult Index()
-        {
-            return View();
+        {   
+            //TODO what is double question mark
+            //FIRST BELOW IS FROM SESSION AND IF IT IS NULL CREATE A NEW LIST OF CART ITEMS
+            //TODO DTO is DATA TRANSFER OBJECT CLASSES FROM MODEL HERE DTO'S
+
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+            CartViewModel cartVM = new CartViewModel
+            {
+                CartItems = cart,
+                GrandTotal = cart.Sum(x => x.Price * x.Quantity)
+
+            };
+
+            return View(cartVM);
         }
     }
 }
